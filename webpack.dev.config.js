c1c529d7933679c50cf36bd77ddb2c2cb09ff4eb
vaiginta@gmail.com <vaiginta@gmail.com>
2017-10-08 15:20:44 +0100

    upgrade webpack

--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -3,27 +3,22 @@ var path = require('path');
 
 module.exports = {
     entry     : [
-      'webpack-dev-server/client?http://localhost:8080',
-      'webpack/hot/dev-server',
       "./src/js/"
     ],
     output    : {
-      path: __dirname, filename : 'bundle.js'
+      path: __dirname + '/public', filename : 'bundle.js'
     },
     module    : {
       loaders: [{
         test: /\.js$/,
         exclude: /node_modules/,
-        loaders: ['babel'],
+        loaders: ['babel-loader'],
         include: path.join(__dirname, 'src')
       },
-      { test: /\.scss$/, loader: 'style!css!sass'},
-      { test: /\.css$/, loader: 'style!css' }]
+      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader'},
+      { test: /\.css$/, loader: 'style-loader!css-loader' }]
     },
     devServer : { hot: true },
     devtool   : 'cheap-module-eval-source-map',
-    plugins   : [ new webpack.HotModuleReplacementPlugin() ],
-    inline    : true,
-    progress  : true,
-    colors    : true
+    plugins   : [ new webpack.HotModuleReplacementPlugin() ]
 };
